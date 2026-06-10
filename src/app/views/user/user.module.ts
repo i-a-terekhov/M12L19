5539fd77eb5a510559874ffd1b40b31767f117da
Terekhov Ivan <for-spam32@yandex.ru>
2026-06-10 12:23:26 +0400

    done: верстка и стили для signup и login

--- src/app/views/user/user.module.ts
+++ src/app/views/user/user.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import {RouterModule} from "@angular/router";
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { SignupComponent } from './signup/signup.component';
   ],
   imports: [
     CommonModule,
+    RouterModule,
     UserRoutingModule
   ]
 })
