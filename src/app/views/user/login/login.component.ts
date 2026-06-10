c154c223e89b865802210e315d74e187e4a55139
Terekhov Ivan <for-spam32@yandex.ru>
2026-06-10 12:45:15 +0400

    done: валидация для login

--- src/app/views/user/login/login.component.ts
+++ src/app/views/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/), Validators.required]),
+  })
+
+  constructor() {  }
+
+  ngOnInit() {
+  }
 
 }
