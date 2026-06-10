8cae4de7c224c837673a18a91a0b0687876b5e61
Terekhov Ivan <for-spam32@yandex.ru>
2026-06-10 12:58:32 +0400

    done: валидация для signup

--- src/app/views/user/signup/signup.component.ts
+++ src/app/views/user/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
+
+  signupForm = new FormGroup({
+    name: new FormControl('', [Validators.pattern(/^[А-Я][а-я]+\s*$/), Validators.required]),
+    lastName: new FormControl('', [Validators.pattern(/^[А-Я][а-я]+\s*$/), Validators.required]),
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/), Validators.required]),
+    agree: new FormControl(false, [Validators.required]),
+  })
+
+  constructor() {  }
+
+  ngOnInit() {
+  }
 
 }
